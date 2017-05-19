0e72d1ca2c03399b77488e2d8350d864764158d1
Black_FireFox <blackfirefox0@gmail.com>
2017-05-19 20:11:53 +0200

    New features
    Now it's possible use a dictionary, on keyword found a sound will be played and some things of the original have been improved.

--- pyrarcr.py
+++ pyrarcr.py
@@ -34,7 +34,7 @@ def rc(rf):
   for b in itertools.product(alphabet,repeat=a):
    k="".join(b)
    if rf[-4:]==".rar":
-    print("Trying:",k)
+    print("Trying:",k,end="\r")
     kf=os.popen("unrar t -y -p%s %s 2>&1|grep 'All OK'"%(k,rf))
     tryn+=1
     for rkf in kf.readlines():
@@ -43,10 +43,11 @@ def rc(rf):
       print("Tried combination count:",tryn)
       print("It took",round(time.time()-start,3),"seconds")
       print("Exiting...")
-      time.sleep(2)
+      os.system("paplay Positive.ogg")
+      #time.sleep(2)
       sys.exit(1)
    elif rf[-4:]==".zip" or rf[-3:]==".7z":
-    print("Trying:",k)
+    print("Trying:",k,end="\r")
     kf=os.popen("7za t -p%s %s 2>&1|grep 'Everything is Ok'"%(k,rf))
     tryn+=1
     for rkf in kf.readlines():
@@ -55,17 +56,80 @@ def rc(rf):
       print("Tried combination count:",tryn)
       print("It took",round(time.time()-start,3),"seconds")
       print("Exiting...")
-      time.sleep(2)
+      os.system("paplay Positive.ogg")
+      #time.sleep(2)
       sys.exit(1)
    else:
     print("ERROR: File isnt a RAR, ZIP or 7z file.\nExiting...")
 
+def rcd(rf,d):
+ try:
+  di=open(d,"r")
+  words=di.read()
+  di.close()
+ except:
+  di=open(d,encoding="ISO-8859-1")
+  words=di.read()
+  di.close()
+ start=time.time()
+ tryn=0
+ for line in words.splitlines():
+  k=line
+  if rf[-4:]==".rar":
+   print("Trying:",k,"          ",end="\r")
+   kf=os.popen("unrar t -y -p%s %s 2>&1|grep 'All OK'"%(k,rf))
+   tryn+=1
+   for rkf in kf.readlines():
+    if rkf=="All OK\n":
+     print("Found password:",repr(k))
+     print("Tried combination count:",tryn)
+     print("It took",round(time.time()-start,3),"seconds")
+     print("Exiting...")
+     os.system("paplay Positive.ogg")
+     #time.sleep(2)
+     sys.exit(1)
+  elif rf[-4:]==".zip" or rf[-3:]==".7z":
+   print("Trying:",k,"          ",end="\r")
+   kf=os.popen("7za t -p%s %s 2>&1|grep 'Everything is Ok'"%(k,rf))
+   tryn+=1
+   for rkf in kf.readlines():
+    if rkf=="Everything is Ok\n":
+     print("Found password:",repr(k))
+     print("Tried combination count:",tryn)
+     print("It took",round(time.time()-start,3),"seconds")
+     print("Exiting...")
+     os.system("paplay Positive.ogg")
+     #time.sleep(2)
+     sys.exit(1)
+  else:
+   print("ERROR: File isnt a RAR, ZIP or 7z file.\nExiting...")
+
 #checking if the file exists/running the function
-if len(sys.argv)==2:
+if len(sys.argv)==2 or len(sys.argv)==4:
  if os.path.exists(sys.argv[1]):
-  rc(sys.argv[1])
+  if len(sys.argv)==4:
+   if sys.argv[2]=="d":
+    if os.path.exists(sys.argv[3]):
+     rcd(sys.argv[1],sys.argv[3])
+    else:
+     print("'"+sys.argv[3]+"' doesn't exist.\nExiting...")
+     sys.exit(1)
+   else:
+    print("Usage:",name,"[rar file]")
+    print("Example:",name,"foobar.rar")
+    print("For use dictionary:",name,"[rar file] d [dictionary file]")
+    print("Example:",name,"foo.rar d bar.txt")
+    print("WARNING! The dictionary file must have the keywords one under the other.")
+    print("Example:\nKeyWord1\nw00d\nkey\nwat3r")
+    sys.exit(1)
+  else:
+   rc(sys.argv[1])
  else:
-  print("ERROR: File doesn't exist.\nExiting...")
+  print("ERROR: '"+argv[1]+"' doesn't exist.\nExiting...")
 else:
  print("Usage:",name,"[rar file]")
  print("Example:",name,"foobar.rar")
+ print("For use dictionary:",name,"[rar file] d [dictionary file]")
+ print("Example:",name,"foo.rar d bar.txt")
+ print("WARNING! The dictionary file must have the keywords one under the other.")
+ print("Example:\nKeyWord1\nw00d\nkey\nwat3r")
